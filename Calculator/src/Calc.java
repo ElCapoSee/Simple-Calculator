5b596a7a53c6378694f49a49f3ff94614fc1a71f
ElCapoSee <elcaposee@gmail.com>
2020-10-25 03:01:34 +0600

    Update directory of the programm

---
+++ Calculator/src/Calc.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Calc {
+	public static void main(String args[]) {
+		Readercalc r = new Readercalc("Calculator");
+		r.setVisible(true);// Делает окно видимым
+		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		r.setSize(300,200);// Размер окна
+		r.setResizable(false);// Запрещает менять размер окна
+		r.setLocationRelativeTo(null);// Центрует окно
+	}
+}
